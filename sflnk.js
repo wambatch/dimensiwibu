c728063071fe7350d09cf1be7c1d7615cf2143dc
wambatch <39588962+wambatch@users.noreply.github.com>
2020-02-04 17:32:47 +0700

    Update sflnk.js

--- sflnk.js
+++ sflnk.js
@@ -148,7 +148,7 @@ function auto_safeconvert() {
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
             a_to_ve[i].href = randomPostingan + "?id=" + encryptedUrl;
-            document.getElementById("entry-title").innerHTML = encryptedUrl;
+            document.getElementById("data_url").innerHTML = encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
