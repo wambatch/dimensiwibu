b6b3a2d7ebfb3ccf3abe7004d8bc469d49f45825
wambatch <39588962+wambatch@users.noreply.github.com>
2020-02-04 18:02:40 +0700

    Update sflnk.js

--- sflnk.js
+++ sflnk.js
@@ -147,7 +147,8 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = randomPostingan + "?id=" + encryptedUrl;
+            document.getElementById("data-url").innerHTML =  + encryptedUrl;
+            a_to_ve[i].href = randomPostingan;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
